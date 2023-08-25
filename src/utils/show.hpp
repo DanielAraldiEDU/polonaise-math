4403fb4167d46b036732494452dc70d1517f3c73
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2023-08-25 20:15:22 -0300

    fix: removed unnecessary blank string in `show` utils

--- src/utils/show.hpp
+++ src/utils/show.hpp
@@ -4,14 +4,10 @@ using namespace std;
 
 template<typename T>
 void showStack(Stack<T> &stack) {
-    for (int index = 0; index <= stack.topLevel; index++) {
-        cout << stack.array[index] << " ";
-    }    
+    for (int index = 0; index <= stack.topLevel; index++) cout << stack.array[index];    
 }
 
 template<typename T>
 void showQueue(Queue<T> &queue) {
-    for (int index = queue.start; index <= queue.end; index++) {
-        cout << queue.array[index] << " ";
-    }    
+    for (int index = queue.start; index <= queue.end; index++) cout << queue.array[index];
 }
