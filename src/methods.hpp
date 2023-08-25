fcfb53e4ff8af448f5a87fda3df2340af7c9903d
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2023-08-25 20:39:51 -0300

    feat: added `length` property to Queue `struct`

--- src/methods.hpp
+++ src/methods.hpp
@@ -11,6 +11,7 @@ template<typename T>
 void initializeQueue(Queue<T> &queue) {
     queue.end = -1;
     queue.start = 0;
+    queue.length = 0;
 }
 
 template<typename T>
@@ -31,16 +32,21 @@ void push(Stack<T> &stack, T value) {
 
 template<typename T>
 bool remove(Queue<T> &queue, T &value) {
-    if (queue.start > queue.end) return false;
+    if (queue.length == 0) return false;
     
     value = queue.array[queue.start];
-    queue.start++;
+    const bool isFull = queue.start == LENGTH - 1;
+    queue.start = isFull ? 0 : queue.start + 1;
+    queue.length--;
     return true;
 }
 
 template<typename T>
 void insert(Queue<T> &queue, T value) {
-    if (queue.end == LENGTH - 1) return;
-    queue.end++;
+    if (queue.length == LENGTH) return;
+
+    const bool isFull = queue.start == LENGTH - 1;
+    queue.end = isFull ? 0 : queue.end + 1;
     queue.array[queue.end] = value;
+    queue.length++;
 }
