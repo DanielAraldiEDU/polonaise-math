2369553daec1a8bdd429d3795644d18dcb045872
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2023-08-25 19:39:16 -0300

    feat: added Stack methods

---
+++ src/methods.hpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include "struct.hpp"
+
+using namespace std;
+
+template<typename T> 
+void initialize(Stack<T> &stack) {
+    stack.topLevel = -1;
+}
+
+template<typename T>
+bool pop(Stack<T> &stack, T &value) {
+    if (stack.topLevel < 0) return false;
+    
+    value = stack.array[stack.topLevel];
+    stack.topLevel--;
+    return true;
+}
+
+template<typename T>
+void push(Stack<T> &stack, T value) {
+    if (stack.topLevel == LENGTH - 1) return;
+    stack.topLevel++;
+    stack.array[stack.topLevel] = value;
+}
