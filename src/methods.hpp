680d0150c8faee59cf1fdc5930f71cb41f573240
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2023-08-25 19:43:18 -0300

    feat: added Queue methods

--- src/methods.hpp
+++ src/methods.hpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
-#include "struct.hpp"
+#include "structs.hpp"
 
 using namespace std;
 
 template<typename T> 
-void initialize(Stack<T> &stack) {
+void initializeStack(Stack<T> &stack) {
     stack.topLevel = -1;
+}
+
+template<typename T> 
+void initializeQueue(Queue<T> &queue) {
+    queue.end = -1;
+    queue.start = 0;
 }
 
 template<typename T>
@@ -23,4 +29,20 @@ void push(Stack<T> &stack, T value) {
     if (stack.topLevel == LENGTH - 1) return;
     stack.topLevel++;
     stack.array[stack.topLevel] = value;
+}
+
+template<typename T>
+bool remove(Queue<T> &queue, T &value) {
+    if (queue.start > queue.end) return false;
+    
+    value = queue.array[queue.start];
+    queue.start++;
+    return true;
+}
+
+template<typename T>
+void insert(Queue<T> &queue, T value) {
+    if (queue.end == LENGTH - 1) return;
+    queue.end++;
+    queue.array[queue.end] = value;
 }
