582946269d330a252cb3df3e93108d9c7ba46d6a
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2023-08-25 20:21:54 -0300

    feat: added insert logical to main file

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,21 @@
 using namespace std;
 
 int main() {
+    Queue<char> queue;
+
+    initializeQueue(queue);
+
+    string expression;
+    cout << "Enter a math expression: ";
+    getline(cin, expression);
+
+    if (expression.length() > LENGTH || expression.length() == 0) {
+        cout << "You entered an invalid expression!";
+        return 0;
+    }
+
+    for (int index = 0; index < expression.length(); index++) {
+        if (expression[index] != ' ') insert(queue, expression[index]);
+    }
     return 0;
 }
