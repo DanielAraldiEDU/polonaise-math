01bb51885d911956c672e0e0d9d08211da3d5f08
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2023-08-27 15:12:38 -0300

    refactor: added native methods to unique file

---
+++ src/native.hpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename T> 
+void initialize(Stack<T> &stack) {
+  stack.topLevel = -1;
+}
+
+template<typename T> 
+void initialize(Queue<T> &queue) {
+  queue.end = -1;
+  queue.start = 0;
+  queue.length = 0;
+}
+
+template<typename T>
+bool pop(Stack<T> &stack, T &value) {
+  if (stack.topLevel < 0) return false;
+  
+  value = stack.array[stack.topLevel];
+  stack.topLevel--;
+  return true;
+}
+
+template<typename T>
+void push(Stack<T> &stack, T value) {
+  if (stack.topLevel == LENGTH - 1) return;
+  stack.topLevel++;
+  stack.array[stack.topLevel] = value;
+}
+
+template<typename T>
+bool remove(Queue<T> &queue, T &value) {
+  if (queue.length == 0) return false;
+  
+  value = queue.array[queue.start];
+  const bool isFull = queue.start == LENGTH - 1;
+  queue.start = isFull ? 0 : queue.start + 1;
+  queue.length--;
+  return true;
+}
+
+template<typename T>
+void insert(Queue<T> &queue, T value) {
+  if (queue.length == LENGTH) return;
+
+  const bool isFull = queue.start == LENGTH - 1;
+  queue.end = isFull ? 0 : queue.end + 1;
+  queue.array[queue.end] = value;
+  queue.length++;
+}
