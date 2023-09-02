a1a76cb93c3238bd305e901308388326ca4db92a
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2023-09-02 15:52:52 -0300

    feat: added validation to all expression

---
+++ src/utils/helpers.hpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+void show(Stack<T> stack) {
+  T value;
+  while (pop(stack, value)) cout << value;
+}
+
+template<typename T>
+void show(Queue<T> queue) {
+  T value;
+  while (remove(queue, value)) cout << value;
+}
+
+template<typename T>
+bool isLetter(T value) {
+  return (value >= 65 && value <= 90) || (value >= 97 && value <= 122);
+}
+
+template<typename T>
+bool isNumber(T value) {
+  return value >= 48 && value <= 57;
+}
+
+template<typename T>
+bool isOperator(T value) {
+  return (value >= 40 && value <= 43) || value == 45 || value == 47 || value == 94;
+}
+
+template<typename T>
+bool isOpenParenthese(T value) {
+  return value == 40;
+}
+
+template<typename T>
+bool isCloseParenthese(T value) {
+  return value == 41;
+}
+
+template<typename T>
+bool isParentheses(T value) {
+  return isOpenParenthese(value) || isCloseParenthese(value);
+}
