150f7964c76a51dfc45fb7246f1e0ddf6f45d4fd
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2023-09-01 22:07:10 -0300

    fix: adjusts in precedence no parentheses

--- src/utils/methods.hpp
+++ src/utils/methods.hpp
@@ -14,7 +14,8 @@ void show(Queue<T> queue) {
   while (remove(queue, value)) cout << value;
 }
 
-int getPrecedenceValue(char operatorValue) {
+template<typename T>
+int getPrecedenceValue(T operatorValue) {
   switch(operatorValue) {
     case '^': return 3;
     case '*': return 2;
@@ -23,18 +24,6 @@ int getPrecedenceValue(char operatorValue) {
     case '-': return 1;
     default: return 0;
   }
-}
-
-bool handlePrecedence(char currentOperator, char previousOperator) {
-  int currentOperatorPrecedence = getPrecedenceValue(currentOperator);
-  int previousOperatorPrecedence = getPrecedenceValue(previousOperator);
-
-  const int openParentheses = 40;
-  const int closeParentheses = 41;
-  if (currentOperator == openParentheses || currentOperator == closeParentheses) return true;
-  if (previousOperator == openParentheses || previousOperator == closeParentheses) return true;
-  if (currentOperator == previousOperator) return false;
-  return currentOperatorPrecedence > previousOperatorPrecedence;
 }
 
 template<typename T>
@@ -49,58 +38,63 @@ template<typename T>
 Queue<T> transformPolonaiseNotation(Queue<T> &queue) {
   Queue<T> resultQueue;
   Stack<T> stack;
-  T value, previousValue = NULL;
-  int cont = 1;
+  T queueValue, stackValue;
 
   initialize(stack);
   initialize(resultQueue);
 
-  while(remove(queue, value)) {
-    const bool isOperator = (value >= 40 && value <= 43) || value == 45 || value == 47 || value == 94;
-    const bool isCloseParentheses = value == 41;
-    bool isParentheses = value == 40 || value == 41;
+  while(remove(queue, queueValue)) {
+    const bool isOperator = (queueValue >= 40 && queueValue <= 43) || queueValue == 45 || queueValue == 47 || queueValue == 94;
+    const bool isCloseParentheses = queueValue == 41;
 
     if (isOperator) {
-      bool isCurrentValueBiggest;
-      if (previousValue != NULL) {
-          isCurrentValueBiggest = handlePrecedence(value, previousValue);
-          if (!isCurrentValueBiggest) {
-            while(pop(stack, previousValue) && !isCurrentValueBiggest) {
-              isParentheses = previousValue == 40 || previousValue == 41;
-              isCurrentValueBiggest = handlePrecedence(value, previousValue);
-              if (isParentheses) {
-                isCurrentValueBiggest = true;
-                push(stack, previousValue);
-              } else {
-                insert(resultQueue, previousValue);
-              }
-            }
-            if (value != 41) push(stack, value);
-          } else if (isCloseParentheses) {
-            bool isFoundOpenParentheses = false;
-            bool isFoundValue = pop(stack, value);
-            while (isFoundValue != isFoundOpenParentheses) {
-              isFoundOpenParentheses = value == 40;
-              if (!isFoundOpenParentheses) {
-                insert(resultQueue, value);
-                isFoundValue = false;
-              } else {
-                isFoundValue = pop(stack, value);
-              }
-            }
-          } else {
-            push(stack, value);
-          }
+      if (isCloseParentheses) {
+        
       } else {
-        push(stack, value);
+        bool hasValue = pop(stack, stackValue);
+        if (hasValue) {
+          int precedenceValue = getPrecedenceValue(stackValue);
+          int precedenceQueueValue = getPrecedenceValue(queueValue);
+  
+          if (precedenceQueueValue > precedenceValue) {
+            push(stack, stackValue);
+            push(stack, queueValue);
+          } else {
+            insert(resultQueue, stackValue);
+
+            bool hasNextStackValue = pop(stack, stackValue);
+            if (hasNextStackValue) {
+              while (hasNextStackValue) {
+                precedenceValue = getPrecedenceValue(stackValue);
+                precedenceQueueValue = getPrecedenceValue(queueValue);
+                
+                if (precedenceQueueValue <= precedenceValue) {
+                  insert(resultQueue, stackValue);
+                } else if (precedenceQueueValue > precedenceValue) {
+                  push(stack, stackValue);
+                  push(stack, queueValue);
+                  hasNextStackValue = false;
+                }
+  
+                if (hasNextStackValue) {
+                  hasNextStackValue = pop(stack, stackValue);
+                  if (!hasNextStackValue) push(stack, queueValue);
+                }
+              }
+            } else {
+              push(stack, queueValue);
+            }
+          }
+        } else {
+          push(stack, queueValue);
+        }
       }
-      previousValue = value;
     } else {
-      insert(resultQueue, value);
+      insert(resultQueue, queueValue);
     }
   }
 
-  if (!queue.length) insertOperatorsInQueue(resultQueue, stack, value);
+  if (!queue.length) insertOperatorsInQueue(resultQueue, stack, queueValue);
 
   return resultQueue;
 }
